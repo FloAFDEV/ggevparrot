c14b3d27010da723afddf683cf95af2c0f403ca1
FloAFDEV <103335500+FloAFDEV@users.noreply.github.com>
2024-03-28 17:11:42 +0100

    adjust SearchFilters layout and styling for better responsiv

--- src/components/Global/SearchFilters/SearchFilters.tsx
+++ src/components/Global/SearchFilters/SearchFilters.tsx
@@ -12,28 +12,28 @@ const SearchFilters = ({
 	handlePriceFilterChange: (value: string) => void;
 }) => {
 	return (
-		<div className="flex justify-around mb-6">
+		<div className="flex flex-wrap justify-center mb-6">
 			<input
 				type="text"
 				placeholder="Type de carburant"
 				onChange={(e) => handleFuelTypeFilterChange(e.target.value)}
-				className="p-2 border-2 border-lime-500 rounded-md text-center text-xl"
+				className="p-2 border-2 border-lime-500 rounded-md text-center text-xl mb-2 mx-2 w-full sm:w-auto"
 			/>
 			<input
 				type="text"
 				placeholder="Couleur"
 				onChange={(e) => handleColorFilterChange(e.target.value)}
-				className="p-2 border-2 border-lime-500 rounded-md text-center text-xl"
+				className="p-2 border-2 border-lime-500 rounded-md text-center text-xl mb-2 mx-2 w-full sm:w-auto"
 			/>
 			<input
 				type="number"
 				placeholder="Prix maximum"
 				onChange={(e) => handlePriceFilterChange(e.target.value)}
-				className="p-2 border-2 border-lime-500 rounded-md text-center text-xl"
+				className="p-2 border-2 border-lime-500 rounded-md text-center text-xl mb-2 mx-2 w-full sm:w-auto"
 			/>
 			<button
 				onClick={handleFilter}
-				className="btn btn-secondary text-lg p-2"
+				className="btn btn-secondary text-lg p-2 mx-2"
 			>
 				Appliquer les filtres
 			</button>
