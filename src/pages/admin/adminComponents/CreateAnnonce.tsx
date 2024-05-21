7d853bdb77ffdea7f6f02af5b257d543fb89a4d1
FloAFDEV <103335500+FloAFDEV@users.noreply.github.com>
2024-05-21 22:32:52 +0200

    "Added two new empty React components: CreateAnnonce and DeleteAnnonce"

---
+++ src/pages/admin/adminComponents/CreateAnnonce.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CreateAnnoncePage = () => {
+	return <></>; // Composant vide
+};
+
+export default CreateAnnoncePage;
---
+++ src/pages/admin/adminComponents/DeleteAnnonce.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DeleteAnnoncePage = () => {
+	return <></>; // Composant vide
+};
+
+export default DeleteAnnoncePage;
