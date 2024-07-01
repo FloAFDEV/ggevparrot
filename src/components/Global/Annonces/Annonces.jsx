5b737060ca8d5237317eee0ae0821d099cefda5c
FloAFDEV <103335500+FloAFDEV@users.noreply.github.com>
2024-07-01 13:23:22 +0200

    "Simplified conditional statements in Annonces component"

--- src/components/Global/Annonces/Annonces.jsx
+++ src/components/Global/Annonces/Annonces.jsx
@@ -95,18 +95,14 @@ const Annonces = () => {
 				(kilometersMaxFilter === "" ||
 					annonce.mileage <= parseInt(kilometersMaxFilter)) &&
 				(brandFilter.length === 0 ||
-					brandFilter.some(
-						(brand) =>
-							annonce.brand_name &&
-							brand &&
-							annonce.brand_name
-								.toLowerCase()
-								.includes(brand.toLowerCase())
+					brandFilter.some((brand) =>
+						annonce.brand_name
+							.toLowerCase()
+							.includes(brand.toLowerCase())
 					)) &&
 				(!fuelTypeFilter ||
-					(annonce.fuel_type &&
-						annonce.fuel_type.toLowerCase() ===
-							fuelTypeFilter.toLowerCase()))
+					annonce.fuel_type.toLowerCase() ===
+						fuelTypeFilter.toLowerCase())
 			);
 		});
 		setFilteredAnnonces(filtered);
