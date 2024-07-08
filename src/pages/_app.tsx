9e76f05fc7dcd6b7f36887e9cfe7104359e8ecc5
FloAFDEV <103335500+FloAFDEV@users.noreply.github.com>
2024-07-08 19:49:10 +0200

    "Added cookie consent feature with local storage and updated App component with new state variables, effects, and UI elements"

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
@@ -6,26 +6,47 @@ import { ThemeProvider } from "@/components/Global/Context/ThemeContext";
 import ClientThemeWrapper from "@/components/Global/Context/ClientThemeWrapper";
 import Head from "next/head";
 import { SessionProvider } from "next-auth/react";
+import Logo from "@/components/Global/Logo/Logo";
+import Link from "next/link";
 
 function MyApp({ Component, pageProps }: AppProps) {
 	const router = useRouter();
+	const [hasConsented, setHasConsented] = useState(false);
+	const [hasNoConsented, setHasNoConsented] = useState(false);
 
 	useEffect(() => {
-		// Exécute des actions globales basées sur la navigation
 		console.log("La page a changé :", router.pathname);
 	}, [router.pathname]);
+
+	useEffect(() => {
+		const consent = localStorage.getItem("cookieConsent");
+		if (consent === "true") {
+			setHasConsented(true);
+		} else if (consent === "false") {
+			setHasNoConsented(true);
+		}
+	}, []);
+
+	const handleConsent = () => {
+		localStorage.setItem("cookieConsent", "true");
+		setHasConsented(true);
+		setHasNoConsented(false);
+	};
+
+	const handleNoConsent = () => {
+		localStorage.setItem("cookieConsent", "false");
+		setHasNoConsented(true);
+		setHasConsented(false);
+	};
 
 	return (
 		<SessionProvider session={pageProps.session}>
 			<Head>
-				{/* Titre */}
 				<title>Garage V.PARROT</title>
-				{/* Méta-description */}
 				<meta
 					name="description"
 					content="Garage, réparations, financements, occasions, véhicules, voiture, Vincent PARROT"
 				/>
-				{/* Balises Open Graph pour les réseaux sociaux */}
 				<meta property="og:title" content="Garage V.PARROT" />
 				<meta
 					property="og:description"
@@ -40,7 +61,6 @@ function MyApp({ Component, pageProps }: AppProps) {
 					property="og:image"
 					content="https://ggevparrot.vercel.app/_next/image?url=%2Fassets%2FheroVParrot.jpeg&w=2048&q=75"
 				/>
-				{/* Méta-informations pour les appareils mobiles */}
 				<meta
 					name="viewport"
 					content="width=device-width, initial-scale=1.0"
@@ -48,6 +68,36 @@ function MyApp({ Component, pageProps }: AppProps) {
 			</Head>
 			<ThemeProvider>
 				<ClientThemeWrapper>
+					{!hasConsented && !hasNoConsented && (
+						<div className="fixed bottom-0 left-0 right-0 bg-gray-800 text-white p-4 text-center z-50 flex flex-col sm:flex-row items-center justify-center space-x-4 space-y-4 sm:space-y-0">
+							<Link href="/">
+								<Logo
+									src="/favicon.ico"
+									className="cursor-pointer rounded-full h-16 w-16 sm:h-20 sm:w-20 lg:h-24 lg:w-24 hidden sm:block"
+								/>
+							</Link>
+							<p className="text-center sm:text-left">
+								Ce site utilise des cookies afin de garantir une
+								expérience optimale.
+							</p>
+							<div className="flex space-x-4">
+								<button
+									onClick={handleConsent}
+									className="mt-2 px-4 py-2 bg-info text-gray-800 rounded hover:bg-success transition-colors"
+									aria-label="Accepter les cookies"
+								>
+									Accepter les Cookies
+								</button>
+								<button
+									onClick={handleNoConsent}
+									className="mt-2 ml-4 px-4 py-2 bg-warning-content rounded hover:bg-neutral transition-colors"
+									aria-label="Refuser les cookies"
+								>
+									Refuser les cookies
+								</button>
+							</div>
+						</div>
+					)}
 					<Component {...pageProps} />
 				</ClientThemeWrapper>
 			</ThemeProvider>
